d67e62702df1296135319d48775430e80de6e048
Adham Faraitodi <109128115+adhamfaraitodi@users.noreply.github.com>
2023-08-13 17:40:31 +0700

    update tampilan homepage

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Homapage</title>
+    <title>Homepage</title>
 </head>
 <body>
     <h4> halaman ini akan diisi seputar kegiatan saya</h4>

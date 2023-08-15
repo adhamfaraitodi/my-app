d9c0dfcaf5692546da27684e948d47cfe99b86de
Adham Faraitodi <109128115+adhamfaraitodi@users.noreply.github.com>
2023-08-15 19:04:18 +0700

    fixing header

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,13 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Homepage</title>
-</head>
-<body>
-    <h4> halaman ini akan diisi seputar kegiatan saya</h4>
-    
-</body>
-</html>
+@extends('home.templates.main')
+
+@section('content')
+
+<p>This is the user content</p>
+
+@endsection
